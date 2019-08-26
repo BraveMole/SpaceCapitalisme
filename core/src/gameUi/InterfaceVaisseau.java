85c0da65011831ec76f6759b761cf9a4630d3a3d
BraveMole <bsouchu@gmail.com>
2019-08-27 01:41:47 +0200

    Create InterfaceVaisseau.java

---
+++ core/src/gameUi/InterfaceVaisseau.java
@@ -0,0 +1,9 @@
+package gameUi;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
+public class InterfaceVaisseau extends Interface{
+    public InterfaceVaisseau(Skin skin, float width, float height, float x, float y) {
+        super(skin, width, height, x, y, "InterfaceVaisseau");
+    }
+}
